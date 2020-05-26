4be6f3177c011dce5aa61b2fc4f8b993d01a89a7
Christoph Abert <christoph.abert@telekom.de>
2020-05-26 15:36:10 +0200

    Api Factory adapted

--- MyStuff/app/src/main/java/de/telekom/sea/mystuff/frontend/android/api/ApiFactory.java
+++ MyStuff/app/src/main/java/de/telekom/sea/mystuff/frontend/android/api/ApiFactory.java
@@ -7,6 +7,8 @@ import androidx.annotation.Nullable;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.security.cert.Certificate;
 import java.util.Collection;
 
@@ -50,14 +52,12 @@ public class ApiFactory {
         this.baseRestUrl = protocol + "://" + hostName + ":" + port;
         this.hostName = hostName;
 
-        TokenAuthenticator authenticator = new TokenAuthenticator(this);
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
 
         OkHttpClient okHttpClient;
         // create OkHttp client
         okHttpClient = new OkHttpClient.Builder()
-                    .authenticator(authenticator)
                     .addInterceptor(loggingInterceptor)
                     .build();
 
@@ -88,104 +88,5 @@ public class ApiFactory {
         return this.baseRestUrl;
     }
 
-    private static class TokenAuthenticator implements Authenticator {
-
-        static final int HTTP_UNAUTHORIZED = 401;
-        static final int MAX_RETRY = 5;
-        private final ApiFactory apiFactory;
-        String authToken = null;
-
-        private TokenAuthenticator(ApiFactory factory) {
-            this.apiFactory = factory;
-        }
-
-        @Override
-        public Request authenticate(@Nullable Route route, @NotNull Response response) {
-
-            int retry = responseCount(response);
-            // retrieve authenticationApi and update refreshTokens
-            AuthenticationApi authenticationApi = SmartCredentialsAuthenticationFactory.getAuthenticationApi();
-            authenticationApi.performActionWithFreshTokens(new OnFreshTokensRetrievedListener() {
-
-                @Override
-                public void onRefreshComplete(@Nullable String accessToken, @Nullable String idToken, @Nullable AuthorizationException exception) {
-                    if (exception != null) {
-                        if ("Invalid refresh_token".equals(exception.getErrorDescription())) {
-                            // Refresh token invalid - New login required!
-                            Intent intent = new Intent();
-                            intent.setAction(MayoBroadcastActions.MAYO_ON_REFRESH_TOKEN_INVALID.toString());
-                            intent.putExtra(MayoBroadcastReceiver.KEY_EXTRA, exception.getErrorDescription());
-                            TokenAuthenticator.this.apiFactory.broadcastSender.sendBroadcast(intent);
-                        } else {
-                            // Other token related problem
-                            Timber.e("Retry %s: Authorization failed! %s", retry, exception.getErrorDescription());
-                        }
-                    } else {
-                        // valid token available - use obtained token
-                        if ("TELEKOM".equalsIgnoreCase(TokenAuthenticator.this.apiFactory.identityProviderId)) {
-                            authToken = accessToken;
-                        } else {
-                            authToken = idToken;
-                        }
-
-                        JwtConsumer jwtConsumer = new JwtConsumerBuilder()
-                                .setSkipAllValidators()
-                                .setSkipSignatureVerification().build();
-                        try {
-                            Timber.d("Retry %s: Token Expiration time = %s", retry, jwtConsumer.processToClaims(authToken).getExpirationTime());
-                            Timber.d("Retry %s: AuthToken = %s", retry, authToken);
-                        } catch (Exception e) {
-                            Timber.e(e, "Retry %s: Invalid JWT of token!", retry);
-                        }
-                    }
-
-                }
-
-                @Override
-                public void onFailed(AuthenticationError errorDescription) {
-                    // this branch is never reached
-                }
-            });
-
-            // Authentication is required , but we have no auth token:
-            // Retry a few times; might be network problem during token refresh
-            if (authToken == null) {
-                Timber.e("Retry %s: Bearer authentication failed - no auth token available: %s", retry, response);
-                if (retry > MAX_RETRY) {
-                    return null;
-                }
-            } else {
-                // We have an auth token; check what went wrong in the previous request:
-                // 2) we failed already with the current authToken
-                String authHeaderUsedInFormerRequest = response.request().header("Authorization");
-                if ((response.code() == TokenAuthenticator.HTTP_UNAUTHORIZED) &&
-                        (authHeaderUsedInFormerRequest != null) &&
-                        authHeaderUsedInFormerRequest.equals("Bearer " + authToken)) {
-                    Timber.e("Retry %s: Bearer authentication failed - token is not accepted by backend: %s", retry, response);
-                    return null;
-                }
-
-                // 3) we had a non-authentication related error in the last call
-                if (response.code() == 400 || response.code() > 401) {
-                    Timber.e("Retry %s: Rest call failed, http response status <> 401: %s", retry, response);
-                    return null;
-                }
-            }
-
-            // return a new request with a fresh authToken
-            return response.request().newBuilder()
-                    .header("Authorization", "Bearer " + authToken)
-                    .build();
-
-        }
-
-        private int responseCount(Response response) {
-            int result = 1;
-            while ((response = response.priorResponse()) != null) {
-                result++;
-            }
-            return result;
-        }
-    }
 
 }
